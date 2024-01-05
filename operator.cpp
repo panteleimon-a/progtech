7e313e5a63de394e22773807be00ac92a08f5081
panteleimon-a <pantelisagg1@gmail.com>
2024-01-05 17:46:55 +0200

    Finalizing notes on ProgTech Kat2023-24

--- operator.cpp
+++ operator.cpp
@@ -2,35 +2,44 @@
 using namespace std;
 class Fraction {
     public:
-    Fraction(int num , int den){
+    Fraction(int num=0 , int den=1){
         this->num = num;
         this->den = den;
     }
-    
-    Fraction& operator+=(const Fraction &rhs){
-        this->num = rhs.den * this->num +this->den * rhs.num;
-        this->den = this->den * rhs.den;
-        return *this;
+    Fraction& operator++()
+    {
+    num += den; return *this;
     }
-        void print(){
-            cout << num << "/" << den << endl;
-        } 
-        private:
-            int num;
-            int den; 
-            };
+    Fraction operator++(int)
+    {
+    num += den;
+    Fraction temp(num, den);
+    return temp; 
+    }
 
-int main() { 
-    Fraction a(1,2); 
-    Fraction c(1,1);
-    cout << "a is ";
-    a.print();
-    cout << endl;
-    cout << "c is ";
-    c.print();
-    cout << endl;
-    c += a;
-    cout << "c += a is ";
-    c.print();
-    return 0;
-    }
+    void print(){
+        cout << num << "/" << den << endl;
+    } 
+    private:
+        int num;
+        int den; 
+        };
+
+int main() {
+Fraction a(1,2);
+Fraction c;
+cout << "a is ";
+a.print();
+cout << endl;
+cout << "Prefix Increment" << endl; c = ++a;
+cout << "c is ";
+c.print();
+cout << "a is ";
+a.print();
+cout << endl;
+cout << "Postfix Increment" << endl; c = a++;
+cout << "c is ";
+c.print();
+cout << "a is "; a.print();
+return 0;
+}
